4673aa37ae4e59841cdc4959e3c8ee641f842f31
Aditya Singh <adityasingh222247@gmail.com>
2018-09-11 10:06:37 +0530

    initial build commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Aux from '../../hoc/aux1'
+
+const layout = (props) => {
+    return
+    <Aux>
+        <div>Toolbar, Sidedrawer, BackDrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+
+}
