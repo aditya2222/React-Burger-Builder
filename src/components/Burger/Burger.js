bd68f7fce9f6cea522849ca079faf9815212030b
Aditya Singh <adityasingh222247@gmail.com>
2018-09-11 16:20:09 +0530

    add ingredients mesasge

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map((igKey, index) => {
+    let transformedIngredients = Object.keys(props.ingredients).map((igKey, index) => {
         return [...Array(props.ingredients[igKey])].map((_, index) => {
             return <BurgerIngredient key={igKey + index} type={igKey} />;
         })
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
