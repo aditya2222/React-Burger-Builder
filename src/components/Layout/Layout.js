a82ec0043b371e4c294cbba0550847c3dd89f5c1
Aditya Singh <adityasingh222247@gmail.com>
2018-09-11 11:55:30 +0530

    BurgerBuilder Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Aux from '../../hoc/aux1'
 
 const layout = (props) => {
-    return
+    return(
     <Aux>
         <div>Toolbar, Sidedrawer, BackDrop</div>
         <main>
             {props.children}
         </main>
     </Aux>
-
+    )
 }
+
+export default layout
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/aux1'
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder
