e23d767258e304a562437c498f21029bbebd19cb
Aditya Singh <adityasingh222247@gmail.com>
2018-09-12 13:53:44 +0530

    Multiple Build Controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+    {
+        label: 'Salad',
+        type: 'salad'
+    },
+    {
+        label: 'Bacon',
+        type: 'bacon'
+    },
+    {
+        label: 'Cheese',
+        type: 'cheese'
+    },
+    {
+        label: 'Meat',
+        type: 'meat'
+    },
+]
 
 const buildControls = (props) => {
     return (
 
-        <div>
-                
+        <div className={classes.BuildControls}>
+            {controls.map((el, index) => {
+                return (<BuildControl key={el.label} label={el.label} />)
+            })}
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/aux1'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -15,7 +16,7 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <div>
-                    Build Controls
+                    <BuildControls />
                 </div>
             </Aux>
         )
