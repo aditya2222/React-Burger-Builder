262f028a9fa5200b9b828f1179507e502e254519
Aditya <adityasingh222247@gmail.com>
2018-10-06 16:21:07 +0530

    Total Price Calculator

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
-import reducer from './store/reducers'
+import reducer from './store/reducer'
 
 
 const store = createStore(reducer);
