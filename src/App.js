52635b19badd9279e863ecc2239799a5fd9e3052
Aditya <adityasingh222247@gmail.com>
2018-10-14 18:27:12 +0530

    Refactoring reducers continued

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout'
+import Checkout from  './containers/Checkout/Checkout'
 import {Route, Switch} from 'react-router-dom'
 import Orders from './containers/Orders/Orders'
 
