cb01872e59a287f053c4b4be603c65960ac269ee
Aditya <adityasingh222247@gmail.com>
2018-10-23 21:14:18 +0530

    Displaying user specific orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -113,7 +113,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
 
         };
 
@@ -213,6 +214,7 @@ const mapStateToProps = (state) => {
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
         token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
