fbd61893baf85643d7eb776a8b4af56cf0adbede
Aditya <adityasingh222247@gmail.com>
2018-10-27 15:38:31 +0530

    Testing components continued

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,18 @@ configure({
 })
 
 describe('<NavihationnItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    })
     it('should render 2 navigation items if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />)
         expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+
+    it('should render 3 navigation items if authenticated', () => {
+        wrapper.setProps({
+            isAuthenticated: true
+        })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
     })
 })
