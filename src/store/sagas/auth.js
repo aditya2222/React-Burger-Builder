e8d9aab70b1b29133885d237936eacf75e66dcd9
Aditya <adityasingh222247@gmail.com>
2018-11-04 13:58:49 +0530

    Handling authentication with saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,6 +1,7 @@
 import { delay } from 'redux-saga'
 import { put } from 'redux-saga/effects'
 import * as actions from '../actions/index'
+import axios from 'axios'
 
 // The star "*" after keyword function converts this into a generator and these can be paused
 // These donot execute from start to end immediately
@@ -15,4 +16,31 @@ export function* logoutSaga(action) {
 export function* checkAuthTimeoutSaga(action) {
     yield delay(action.expirationTime * 1000)
     yield put(actions.logout())
+}
+
+export function* authUserSaga(action) {
+		yield put(actions.authStart())
+        const authData = {
+            email: action.email,
+            password: action.password,
+            returnSecureToken: true
+        }
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBMuwBq3yKQtJjIno05M6Vo4WesEWqM8Tk'
+        if (!action.isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBMuwBq3yKQtJjIno05M6Vo4WesEWqM8Tk'
+        }
+        try{
+
+    const response = yield axios.post(url, authData)
+	    const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000)
+	    yield localStorage.setItem('token', response.data.idToken);
+	    yield localStorage.setItem('expirationDate', expirationDate);
+	    yield localStorage.setItem('userId', response.data.localId)
+	    yield put(actions.authSuccess(response.data.idToken, response.data.localId))
+	    yield put(actions.checkAuthTimeout(response.data.expiresIn))
+	}
+	catch (error) {
+		yield put(actions.authFail(error.response.data.error))
+	}
+       
 }
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,12 @@
 import { takeEvery } from "redux-saga";
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga, checkAuthTimeoutSaga } from "./auth";
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from "./auth";
 
 // the function below is a generator
 
 export function* watchAll(action) {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
+    yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
 }
 
