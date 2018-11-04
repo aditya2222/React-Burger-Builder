884da032c648a1b39e4322df71facd7ca70941f9
Aditya <adityasingh222247@gmail.com>
2018-11-04 13:04:26 +0530

    Adding more logic to sagas

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,14 +1,18 @@
+import { delay } from 'redux-saga'
 import { put } from 'redux-saga/effects'
-import * as actionTypes from '../actions/actionTypes'
+import * as actions from '../actions/index'
 
 // The star "*" after keyword function converts this into a generator and these can be paused
 // These donot execute from start to end immediately
-export function* logoutSaga(action){
-	// yiels means wait to execute and finish
-	yield localStorage.removeItem('token')
+export function* logoutSaga(action) {
+    // yiels means wait to execute and finish
+    yield localStorage.removeItem('token')
     yield localStorage.removeItem('expirationDate')
     yield localStorage.removeItem('localId')
-    yield put({
-    	type: actionTypes.AUTH_LOGOUT
-    })
+    yield put(actions.logoutSucceed())
+}
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(action.expirationTime * 1000)
+    yield put(actions.logout())
 }
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,11 @@
 import { takeEvery } from "redux-saga";
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga } from "./auth";
+import { logoutSaga, checkAuthTimeoutSaga } from "./auth";
 
 // the function below is a generator
 
 export function* watchAll(action) {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
 }
 
